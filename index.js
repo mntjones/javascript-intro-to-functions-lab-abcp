19311e5e51e0901dee42ff92c8dd1bbddb0c7602
Monica Tamaru Jones <mntamaru@gmail.com>
2018-01-13 17:47:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return sting.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(shout(string))
+}
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
+function sayHiToGrandma(string) {
+  if (string === whisper(string)) {
+    return "I can't hear you!"
+  }
+  
+  
+}
